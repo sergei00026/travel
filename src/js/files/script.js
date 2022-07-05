dd1243ea14d6ca4fac5617699e50f35d9f7151ab
sergei00026 <sergei57fx@mail.ru>
2022-07-05 18:18:22 +0500

    fix: added smooth scrolling

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,19 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+// Плавный скролл
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault()
+
+		const blockID = anchor.getAttribute('href').substr(1)
+
+		document.getElementById(blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		})
+	})
+}
